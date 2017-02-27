8301b44d11ee67be6c29cdc008aa5953bf2b8984
Chad Swatsworth <Chad Swatsworth>
2017-02-27 16:01:48 -0500

    check for empty position

--- test_board.rb
+++ test_board.rb
@@ -5,7 +5,7 @@ class TestTicTacToe < Minitest::Test
 	
 	def test_new_board
 		board = Board.new
-		assert_equal(Array.new(9, ''),board.ttt_board)
+		assert_equal(Array.new(9, ''), board.ttt_board)
 	end
 
 	def test_update_first_position_x_or_o
@@ -20,4 +20,17 @@ class TestTicTacToe < Minitest::Test
 		board.update_position(1, 'O')
 		assert_equal(['X','O','','','','','','',''], board.ttt_board)
 	end
-end
+
+	def test_update_partial_board_at_last_position
+		board = Board.new
+		board.ttt_board = ['X','O','','O','','','','','']
+		board.update_position(8, 'X')
+		assert_equal(['X','O','','O','','','','','X'], board.ttt_board)
+	end
+
+	def test_check_if_position_is_avaliable
+		board = Board.new
+		board.ttt_board = ['X','O','','O','','','','','']
+		assert_equal(true, board.valid_position?(6))
+	end
+end	
