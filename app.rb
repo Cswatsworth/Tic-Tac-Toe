23e29960a9c39e4b5945cb4cf3dd02542fef6a01
Chad R Swatsworth <Chad@Chads-MacBook-Pro.local>
2017-03-15 23:47:25 -0400

    do not have a functional board that is displayed not sure why

--- app.rb
+++ app.rb
@@ -1,23 +1,46 @@
 require 'sinatra'
+require_relative 'board.rb'
+require_relative 'random_AI.rb'
+require_relative 'sequential_AI.rb'
+require_relative 'unbeatable.rb'
+
 
 class TicTacToeApp < Sinatra::Base
 		
-	get '/' do
+		get '/' do
+		
 		erb :greeting
-	end
 
-	post '/greeting' do
-		greeting = params[:greeting_input]
-		redirect '/play_game?greeting'
-	end
-
-	get '/play_game' do 
-		greeting = params[:greeting]
-		erb :play_game, locals: {greeting: greeting}
 	end
 
 	post '/play_game' do
-		greeting = params[:greeting]
-		play_game = params[:play_game_input]
+		session[:opponent] = params[:opponent]
+		board = Board.new
+		
+		erb :play_game, :locals => {opponent: session[:opponent], board: board}
+
 	end
+
+
+	# get '/' do
+	# 	erb :greeting
+	# end
+
+	# post '/greeting' do
+	# 	greeting = params[:greeting_input]
+	# 	redirect '/play_game?'
+	# end
+
+	# get '/play_game' do 
+	# 	greeting = params[:greeting]
+	# 	erb :play_game, locals: {greeting: greeting, board: board}
+	# end
+
+	# post '/play_game' do
+	# 	greeting = params[:greeting]
+	# 	play_game = params[:play_game]
+	# 	opponent = params[:opponent]
+		
+		
+	# end
 end
